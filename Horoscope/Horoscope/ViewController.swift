58a8207dda49bfd850a04cac7b75eb3752e6afd7
Ruslan Latfulin <latfulinruslan@MacBook-Pro-aaaa.local>
2019-01-13 16:29:20 +0300

    First commit.

---
+++ Horoscope/Horoscope/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Horoscope
+//
+//  Created by Ruslan Latfulin on 13.01.2019.
+//  Copyright © 2019 Ruslan Latfulin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
